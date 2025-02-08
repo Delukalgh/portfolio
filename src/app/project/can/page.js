2fbf940d60705a145b95aa5a5fff4456c25f37b2
Deluka <deluka.lgh@gmail.com>
2025-02-07 20:46:00 -0800

    adding parallax to cans project page

--- src/app/project/can/page.js
+++ src/app/project/can/page.js
@@ -1,3 +1,5 @@
+'use client'
+import { useRef, useEffect } from 'react'
 import Image from "next/image";
 import Header from "../../../../components/Header";
 import Cans from "/public/images/cans.png"
@@ -5,8 +7,57 @@ import Cedar from "/public/images/cedarmock.png"
 import Pine from "/public/images/pinemock.png"
 import Maple from "/public/images/maplemock.png"
 import styles from "./Can.module.css";
+import { useScroll, motion, useTransform, useSpring } from "framer-motion"
 
 const Can = () => {
+
+    const cedarRef = useRef();
+    const pineRef = useRef();
+    const mapleRef = useRef();
+
+    const { scrollYProgress: cedarProgress } = useScroll({
+        target: cedarRef,
+        offset: ["start start", "end end"]
+    });
+
+    const { scrollYProgress: pineProgress } = useScroll({
+        target: pineRef,
+        offset: ["start start", "end end"]
+    });
+
+    const { scrollYProgress: mapleProgress } = useScroll({
+        target: mapleRef,
+        offset: ["start start", "end end"]
+    });
+
+    const scaleCedar = useSpring(useTransform(cedarProgress, [0, 1], [1, 0.8]), {
+        stiffness: 100,  // Controls the resistance (higher = snappier)
+        damping: 15,     // Controls how much the motion slows down (higher = smoother)
+        mass: 0.5        // Adjusts inertia (lower values make it feel lighter)
+    });
+    const rotateCedar = useSpring(useTransform(cedarProgress, [0, 1], [0, -5]), {
+        stiffness: 100,
+        damping: 15
+    });
+
+    const scalePine = useSpring(useTransform(pineProgress, [0, 1], [1, 0.8]), {
+        stiffness: 100,
+        damping: 15
+    });
+    const rotatePine = useSpring(useTransform(pineProgress, [0, 1], [0, -5]), {
+        stiffness: 100,
+        damping: 15
+    });
+
+    const scaleMaple = useSpring(useTransform(mapleProgress, [0, 1], [1, 0.8]), {
+        stiffness: 100,
+        damping: 15
+    });
+    const rotateMaple = useSpring(useTransform(mapleProgress, [0, 1], [0, -5]), {
+        stiffness: 100,
+        damping: 15
+    });
+
     return (
         <>
             <Header />
@@ -31,31 +82,46 @@ const Can = () => {
                     <p className="font-light text-[22px]">Evergreen Brewing is a brewery deeply inspired by the rich, earthy essence of the great outdoors. Created by utilizing Adobe Photoshop and Illustrator. This design uses a wood texture with a dark background to make the typography more prominent. It all ties into together with the colors of the pictures matching the typography color.  </p>
                 </div>
 
-                <div className=" flex flex-col p-4 gap-[3em]">
-                    <a>
+                <div className=" flex flex-col p-4 gap-[3em] relative h-[310vh]">
+                    <motion.div
+                        ref={cedarRef}
+                        style={{ scale: scaleCedar, rotate: rotateCedar }}
+                        className="sticky top-0 h-screen"
+                    >
+                        <div className='flex mt-[10em]' />
                         <Image
                             src={Cedar}
                             alt="cedar"
                             width={1000}
                             height={1000}
                         />
-                    </a>
-                    <a>
-                        <Image 
+                    </motion.div>
+                    <motion.div
+                        ref={pineRef}
+                        style={{ scale: scalePine, rotate: rotatePine }}
+                        className="sticky top-0 h-screen"
+                    >
+                        <div className='flex mt-[10em]' />
+                        <Image
                             src={Pine}
                             alt="pine"
                             width={1000}
                             height={1000}
                         />
-                    </a>
-                    <a>
-                        <Image 
+                    </motion.div>
+                    <motion.div
+                        ref={mapleRef}
+                        style={{ scale: scaleMaple, rotate: rotateMaple }}
+                        className="sticky top-0 h-screen"
+                    >
+                        <div className='flex mt-[10em]' />
+                        <Image
                             src={Maple}
                             alt="maple"
                             width={1000}
                             height={1000}
                         />
-                    </a>
+                    </motion.div>
                 </div>
             </div>
         </>
--- src/app/project/page.js
+++ src/app/project/page.js
@@ -1,22 +1,53 @@
+'use client'
+import { useRef, useEffect } from 'react'
 import next from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
 import Cans from '/public/images/cans.png'
+import Blue from '/public/images/blue.png'
 import React from 'react'
+import { useScroll, motion, useTransform } from "framer-motion"
+
 
 const Projects = () => {
+    const container = useRef();
+    const { scrollYProgress } = useScroll({
+        target: container,
+        offset: ["start start", "end end"]
+    })
+    useEffect(() => {
+    }, [])
+
+    const scale = useTransform(scrollYProgress, [0, 1], [1, 0.8]);
+    const rotate = useTransform(scrollYProgress, [0, 1], [0, -5]);
+
     return (
-        <div className="work-links m-auto ">
-            <div className="p-4">
+        <div ref={container} className="flex flex-col work-links m-auto items-center relative h-[200vh]">
+            <motion.div
+                style={{ scale, rotate }}
+                className="sticky top-0 p-4 h-screen"
+            >
                 <a href="/project/can">
                     <Image
                         src={Cans}
                         alt="cans"
-                        width={600}
-                        height={600}
+                        width={1200}
+                        height={1200}
                     />
                 </a>
 
+            </motion.div>
+            <div
+                className="relative p-4 h-screen">
+                <a href="/project/cigbox"
+                >
+                    <Image
+                        src={Blue}
+                        alt='Blue Cigbox'
+                        width={'900'}
+                        height={'900'}
+                    />
+                </a>
             </div>
 
         </div>
