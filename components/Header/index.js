f01991fff331e67b1b4dd1b94107122c35bfba80
Deluka <deluka.lgh@gmail.com>
2025-02-07 16:07:24 -0800

    linking Logo to home page

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,24 +3,27 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./Header.module.css";
+import Home from "@/app/page";
 
 export default function Header() {
     return (
         <>
             <header className={styles.header}>
                 <div className={styles.logo}>
-                    <Image 
-                        src="/images/SVG/logo.svg"
-                        width={150}
-                        height={150}
-                        alt="logo"
-                    />
+                    <a href="/">
+                        <Image
+                            src="/images/SVG/logo.svg"
+                            width={150}
+                            height={150}
+                            alt="logo"
+                        />
+                    </a>
                 </div>
                 <div className={styles.navLinks}>
                     <ul className="">
-                    <li> <Link href="/project">Projects </Link></li>
-                    <li> <Link href="/project">About </Link></li>
-                    <li> <Link href="/contact">Contact </Link></li>
+                        <li> <Link href="/project">Projects </Link></li>
+                        <li> <Link href="/project">About </Link></li>
+                        <li> <Link href="/contact">Contact </Link></li>
                     </ul>
                 </div>
             </header>
--- src/app/project/can/page.js
+++ src/app/project/can/page.js
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import Header from "../../../../components/Header";
 import Cans from "/public/images/cans.png"
+import Cedar from "/public/images/cedarmock.png"
+import Pine from "/public/images/pinemock.png"
+import Maple from "/public/images/maplemock.png"
+import styles from "./Can.module.css";
 
 const Can = () => {
     return (
         <>
             <Header />
-            <div className="flex flex-col items-center justify-center">
+            <div className={styles.container}>
                 <div>
                     <h1 className="font-bold text-[4em]">EVERGREEN BREWING</h1>
                 </div>
@@ -25,6 +29,33 @@ const Can = () => {
                 <div className="flex flex-col mt-[1em] w-[57%] gap-[1em]">
                     <h1 className="flex justify-center font-bold text-[2em]">Package Design</h1>
                     <p>Evergreen Brewing is a brewery deeply inspired by the rich, earthy essence of the great outdoors. Created by utilizing Adobe Photoshop and Illustrator. This design uses a wood texture with a dark background to make the typography more prominent. It all ties into together with the colors of the pictures matching the typography color.  </p>
+                </div>
+
+                <div className=" flex flex-col p-4 gap-[3em]">
+                    <a>
+                        <Image
+                            src={Cedar}
+                            alt="cedar"
+                            width={1000}
+                            height={1000}
+                        />
+                    </a>
+                    <a>
+                        <Image 
+                            src={Pine}
+                            alt="pine"
+                            width={1000}
+                            height={1000}
+                        />
+                    </a>
+                    <a>
+                        <Image 
+                            src={Maple}
+                            alt="maple"
+                            width={1000}
+                            height={1000}
+                        />
+                    </a>
                 </div>
             </div>
         </>
