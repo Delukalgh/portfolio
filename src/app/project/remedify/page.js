684ad43b214188791424fe67df16dfc41a0c574b
Deluka <deluka.lgh@gmail.com>
2025-02-10 02:34:40 -0800

    addding content to remedify page

--- src/app/project/remedify/page.js
+++ src/app/project/remedify/page.js
@@ -2,6 +2,8 @@ import Header from "../../../../components/Header"
 import styles from "./Remedify.module.css";
 import Image from "next/image";
 import Cover from "/public/images/cover.jpg"
+import Auto from "/public/images/automated.jpg"
+import Info from "/public/images/insight.jpg"
 
 const Remedify = () => {
     return (
@@ -51,16 +53,57 @@ const Remedify = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-col w-[57%] mt-[3em]">
+                <div className="flex flex-col w-[57%] mt-[3em] gap-[2em]">
                     <h1 className="flex justify-start font-bold text-[3em] text-[#FF5F00]">Reason</h1>
                     <p className="font-light text-[22px]">
-                        Sure! Here's a refined version with a fresh wording approach:
-                        Studies show that only about 50% of patients with chronic illnesses take their medications as prescribed. 
-                        The two main obstacles? Misunderstanding instructions and forgetfulness.<br></br>
-                        Remedify is an AI-powered medication management app designed to improve adherence and simplify health routines. 
-                        More than just a digital pill organizer, it’s tailored for individuals managing multiple prescriptions or cognitive 
-                        challenges—where the risk of missed or incorrect doses is high. With a strong emphasis on accessibility, Remedify not only 
-                        provides smart reminders but also offers a comprehensive medication library, ensuring users stay informed and on track with their treatment.</p>
+                        Studies show that only about 50% of patients with chronic illnesses take their medications as prescribed.
+                        The two main obstacles? Misunderstanding instructions and forgetfulness.
+                    </p>
+                    <span className="font-light text-[22px]">
+                        Remedify is an AI-powered medication management app designed to improve adherence and simplify health routines.
+                        More than just a digital pill organizer, it’s tailored for individuals managing multiple prescriptions or cognitive
+                        challenges—where the risk of missed or incorrect doses is high. With a strong emphasis on accessibility, Remedify not only
+                        provides smart reminders but also offers a comprehensive medication library, ensuring users stay informed and on track with their treatment.
+                    </span>
+                </div>
+
+                <div className="flex flex-col w-[57%] mt-[3em]">
+                    <h1 className="flex justify-start font-bold text-[3em] text-[#FF5F00]">Main Features</h1>
+                    <div className="flex flex-col gap-[3em]">
+                        <p className="font-light text-[22px]">Automated Scanning</p>
+                        <Image
+                            src={Auto}
+                            alt="automated scanning"
+                            width={1000}
+                            height={1000}
+                        />
+                    </div>
+                    <div className="flex flex-col mt-[8px]">
+                        <p className="font-light text-[22px]">Automated scanning for ease of use referencing information from the Canadian Drug Product Database.</p>
+                        <p className="font-light text-[22px]">For the Azure AI Vision OCR to work image data is uploaded uing blob storage.</p>
+                    </div>
+                    <div className="flex flex-col gap-[3em] mt-[3em]">
+                        <p className="font-light text-[22px]">Generated Information</p>
+                        <Image
+                            src={Info}
+                            alt="generated information"
+                            width={1000}
+                            height={1000}
+                        />
+                    </div>
+                    <div className="flex flex-col mt-[8px] gap-[2em]">
+                        <p className="font-light text-[22px]">AI genearted information about medication such as side effects are fetched from the Canadian Drug Product Database.</p>
+                        <p className="font-light text-[22px]">OpenAI's GPT-4o mini is being used to analyze text data from the OCR and put into the corresponding objects. It's also being
+                            used to generate information on medications using the data that is being fetched the Canadian Drug Product Database.
+                        </p>
+                    </div>
+
+                </div>
+                <div className="flex flex-col w-[57%] mt-[3em] gap-[2em]">
+                    <h1 className="flex justify-start font-bold text-[3em] text-[#FF5F00]">Research</h1>
+                    <p className="font-light text-[22px]">Numerous surveys and interviews were run by the members of the team to ensure that the idea of the app is refined
+                        and validated.
+                    </p>
                 </div>
             </div>
         </>
