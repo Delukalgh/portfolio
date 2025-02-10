1aac3e90a9e988690b185aada74223c71d33ebb9
Deluka <deluka.lgh@gmail.com>
2025-02-10 01:26:59 -0800

    adding links to remedify

--- src/app/project/remedify/page.js
+++ src/app/project/remedify/page.js
@@ -23,6 +23,19 @@ const Remedify = () => {
                     </a>
                 </div>
 
+                <div className="flex flex-col mt-[1em] w-[57%] gap-[5em]">
+                    <h1 className="flex justify-center font-bold text-[3em] text-[#FF5F00]">Medication Reminder App</h1>
+                    <p className="font-light text-[22px]">Remedify is an app designed with accessibility in mind to improve medical adherance rate.</p>
+                    <div className="flex flex-col gap-[2em]">
+                        <h1 className="flex justify-start font-bold text-[3em] text-[#FF5F00]">Additionale Links</h1>
+                        <div className="flex flex-row gap-[4em] font-medium text-[18px]">
+                            <a href="https://www.remedify.ca/">Remedify Website</a>
+                            <a href="https://remedify-blog.vercel.app/">Remedify Blog</a>
+                            <a href="https://github.com/yeenathan/asclepius">Remeify Repo</a>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </>
     )
